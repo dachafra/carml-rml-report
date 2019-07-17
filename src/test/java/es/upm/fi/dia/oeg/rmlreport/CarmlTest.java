580f59cf18e3c10c3eee7a9a136cc7b5d48e5a04
dachafra <dachafra@gmail.com>
2019-07-17 11:13:29 +0200

    first commit

---
+++ src/test/java/es/upm/fi/dia/oeg/rmlreport/CarmlTest.java
@@ -0,0 +1,20 @@
+package es.upm.fi.dia.oeg.rmlreport;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CarmlTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
